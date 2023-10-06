4426f9ba80cdf359fa063371cd5353298a0dbc72
Julien Poissonnier <julien@caffeine.lu>
2023-10-06 12:16:14 +0200

    API route comments static vs SSR

--- src/pages/json.ts
+++ src/pages/json.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from "astro";
 
+// This is called at build time and a static responds is generated.
+// If we set `output: "server"` in the astro config, it will be rendered at runtime.
 export const GET: APIRoute = async function GET({ params, request }) {
   return new Response(
     JSON.stringify({
