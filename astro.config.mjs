ab1bc98489be86ad30dadeb7b4c3b5ce887990de
Julien Poissonnier <julien@caffeine.lu>
2023-10-06 12:17:40 +0200

    Switch to edge functions for SSR mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
